bc63298a9792bdd27a590547151b5b9dfb8e62f3
Raul Ventura <u201714049@upc.edu.pe>
2022-06-22 11:57:15 -0500

    feat (app bar): Implemented app bar with application views navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,18 @@ import {StudentsService} from "./students/services/students.service";
 import { HomeComponent } from './public/home/home.component';
 import { AboutComponent } from './public/about/about.component';
 import { StudentsComponent } from './students/pages/students/students.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatSortModule} from "@angular/material/sort";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -21,7 +33,21 @@ import { StudentsComponent } from './students/pages/students/students.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule  //Agregamos esto
+    HttpClientModule,  //Agregamos esto
+    MatToolbarModule,
+    MatButtonModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSortModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDividerModule
   ],
   providers: [StudentsService],
   bootstrap: [AppComponent]
