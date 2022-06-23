d502d63dfaecb37068e03eb89eb835b42109a44b
Raul Ventura <u201714049@upc.edu.pe>
2022-06-23 02:38:09 -0500

    feat (security): Implemented Authentication behavior in main view.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "./security/services/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learning-center-with-routing';
+  title = 'learning-center';
+
+  constructor(private router: Router,
+              private authService: AuthService) {}
+
+  getCurrentUserEmail() {
+    let currentUserString = this.authService.getCurrentUser();
+    if (currentUserString) {
+      console.log(`current user: ${ currentUserString}`);
+      let currentUser = JSON.parse(currentUserString);
+      console.log(currentUser);
+      return currentUser.email;
+    } else {
+      return null;
+    }
+  }
+
+  signOut() {
+    this.authService.signOut();
+    this.router.navigate(['home']).then();
+    console.log("Signed Out");
+  }
 }
